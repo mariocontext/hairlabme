40b058c5bb71599777bd66ee6a4f3376be4eda0b
Mario Noble <mario@contextmultimedia.com>
2013-07-01 15:52:29 -0700

    tweaked out images and other content to resize at 640 and under

--- wp-content/themes/theme1164/functions.php
+++ wp-content/themes/theme1164/functions.php
@@ -63,4 +63,14 @@
 	}
 	add_filter('post_class', 'category_id_class');
 	add_filter('body_class', 'category_id_class');
+
+	/* customized functions and filters for hairlab- mario@contextmultimedia.com */
+
+	add_filter( 'post_thumbnail_html', 'remove_width_attribute', 10 );
+	add_filter( 'image_send_to_editor', 'remove_width_attribute', 10 );
+
+	function remove_width_attribute( $html ) {
+	$html = preg_replace( '/(width|height)="\d*"\s/', "", $html );
+	return $html;
+	}
 ?>
--- wp-content/themes/theme1164/sidebar.php
+++ wp-content/themes/theme1164/sidebar.php
@@ -1,8 +1,16 @@
-<div id="sidebar">
+
+<div class="small-screen-hider">
+	<!-- on small screens this parent is set to display:none. this may cause css background content to not download inside with the exception of the fennec browser.  http://timkadlec.com/2012/04/media-query-asset-downloading-results/ -->
+
+	<div id="sidebar">
+	
 
 	<?php if (!is_page(57)){ ?><div class="ttl_sidebar"><?php _e('Learn more About our<br />Special Hair Restoration','theme1164'); ?></div>
 	<?php } ?>
 	<?php if ( ! dynamic_sidebar( 'Sidebar' )) : ?>
 		<!-- Wigitized Sidebar -->
 	<?php endif; ?>
-</div><!--sidebar-->
+	
+	</div><!--sidebar-->
+
+</div><!-- /small-screen-hider -->
