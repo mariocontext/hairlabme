1d361eb5dac47478cde8757264bdea258ae535cc
Mario Noble <mario@contextmultimedia.com>
2013-07-09 17:29:03 -0700

    reverted fitvids code to use foxys

--- wp-content/themes/HLchild/js/scripts.js
+++ wp-content/themes/HLchild/js/scripts.js
@@ -1,10 +1,12 @@
 (function($) {
 
-	    // Target your .container, .wrapper, .post, etc.
-	    $("#left-area").fitVids();
+	 
 
 	    // target tables in primary content area. may need to change to limit scope more if there are conflicts
 	    $(".mobilized-table").stacktable({myClass:'stacktable small-only'});
+
+	       // Target your .container, .wrapper, .post, etc.
+	    //$("#left-area").fitVids();
 
 
 })( jQuery );
